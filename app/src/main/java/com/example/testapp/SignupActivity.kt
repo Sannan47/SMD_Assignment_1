86483765592721d6cf6322f8539cf419ea40d07a
Sannan47 <sannanshakeel04@gmail.com>
2025-09-14 01:51:31 +0500

    Added Signup Screen with navigation. Fixed Splash screen navigation

---
+++ app/src/main/java/com/example/testapp/SignupActivity.kt
@@ -0,0 +1,35 @@
+package com.example.testapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+
+        val backBtn = findViewById<ImageView>(R.id.backBtn)
+        val profileImage = findViewById<RelativeLayout>(R.id.profileImage)
+
+        backBtn.setOnClickListener() {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        profileImage.setOnClickListener() {
+            val intent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
