8c39750732a22faae0a48b6c6865df62a4d787b3
Sannan47 <sannanshakeel04@gmail.com>
2025-09-14 03:19:35 +0500

    Added Login Screen with navigation. Adjusted other navigations too

--- app/src/main/java/com/example/testapp/LoginActivity.kt
+++ app/src/main/java/com/example/testapp/LoginActivity.kt
@@ -2,20 +2,22 @@ package com.example.testapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class StartActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_login)
 
         val signupBtn = findViewById<TextView>(R.id.signupBtn)
         val loginBtn = findViewById<TextView>(R.id.loginBtn)
+        val backBtn = findViewById<ImageView>(R.id.backBtn)
 
         signupBtn.setOnClickListener() {
             val intent = Intent(this, SignupActivity::class.java)
@@ -23,10 +25,17 @@ class StartActivity : AppCompatActivity() {
             finish()
         }
 
-        loginBtn.setOnClickListener() {
-            val intent = Intent(this, LoginActivity::class.java)
+
+        backBtn.setOnClickListener() {
+            val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
             finish()
         }
+
+//        loginBtn.setOnClickListener() {
+//            val intent = Intent(this, HomeActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
     }
 }
